7fd933ea7548fc35961f62747847a0305bebf473
enelesmai <xochitl.selene.fp@gmail.com>
2020-04-20 14:39:27 -0500

    added edit method and flash notifications

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,13 +18,31 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' was created!"
+
+    redirect_to article_path(@article)
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+  
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' was destroyed!"
+
     redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
 end
